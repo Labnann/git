dcec1d6ebd8a0946df813885a21bf1c0b06d4d8c
Labnann <khalid.masum.92@gmail.com>
2022-03-27 20:31:07 +0000

    psuh: show params and config opts
    Signed-off-by: Labnann <khalid.masum.92@gmail.com>

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -2,7 +2,20 @@
 
 int cmd_psuh(int argc, const char **argv, const char *prefix)
 {
+    int i;
+
     printf(_("Hello World!\n"));
+
+    printf(Q_("Your args (there is %d):\n",
+		  "Your args (there are %d):\n",
+		  argc),
+	       argc);
+	for (i = 0; i < argc; i++)
+		printf("%d: %s\n", i, argv[i]);
+
+	printf(_("Your current working directory:\n<top-level>%s%s\n"),
+	       prefix ? "/" : "", prefix ? prefix : "");
+
     return 0;
 
 }

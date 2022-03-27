35808f053aded0c879b85519d4c91868a111aa8a
Labnann <khalid.masum.92@gmail.com>
2022-03-27 21:15:21 +0000

    psuh: display the top of origin/master
    Signed-off-by: Labnann <khalid.masum.92@gmail.com>

--- builtin/psuh.c
+++ builtin/psuh.c
@@ -2,11 +2,16 @@
 #include "config.h"
 #include "wt-status.h"
 
+#include "commit.h"
+#include "pretty.h"
+
 int cmd_psuh(int argc, const char **argv, const char *prefix)
 {
 	int i;
 	struct wt_status status;
 	const char *cfg_name;
+	struct commit *c = NULL;
+	struct strbuf commitline = STRBUF_INIT;
 
 	git_config(git_default_config, NULL);
 	if (git_config_get_string_tmp("user.name", &cfg_name) > 0)
@@ -27,7 +32,14 @@ int cmd_psuh(int argc, const char **argv, const char *prefix)
 
 	wt_status_prepare(the_repository, &status);
 	git_config(git_default_config, &status);
-    printf(_("Your current branch: %s\n"), status.branch);
+	printf(_("Your current branch: %s\n"), status.branch);
+
+	c = lookup_commit_reference_by_name("origin/master");
+
+	if (c != NULL) {
+		pp_commit_easy(CMIT_FMT_ONELINE, c, &commitline);
+		printf(_("Current commit: %s\n"), commitline.buf);
+	}
 
 	return 0;
 }
